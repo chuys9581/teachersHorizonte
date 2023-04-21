f47d90fea90e9c8c79b7e6df01b1336f2ddaa4f8
Jesus Jimenez <chuy.dev.f@gmail.com>
2023-04-21 01:57:33 -0600

    primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+function mostrarElemento() {
+    var elemento = document.getElementById("submenu");
+    elemento.style.display = "block";
+    elemento.style.transition = "4s";
+  }
+
+function ocultarElemento() {
+    var elemento = document.getElementById("submenu");
+    elemento.style.display = "none";
+    elemento.style.transition = "2s";
+
+  }
